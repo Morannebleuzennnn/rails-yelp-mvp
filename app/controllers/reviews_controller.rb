546fa6efca032bbac9cafc1b8911cd8737437143
MoranneBleuzen <moranne.bleuzen@kedgebs.com>
2019-05-15 17:36:43 +0200

    add some modif

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,11 @@
 class ReviewsController < ApplicationController
+  def new
+    @reviews = Review.new
+  end
+
+  def create
+    review = Review.new(restaurant_params)
+    review.save
+    redirect_to reviews_path
+  end
 end
